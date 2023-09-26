6c76fc6238c0f8193feed4d3e95773b756af8085
beokcsin <47828264+beokcsin@users.noreply.github.com>
2023-09-26 11:38:46 +0200

    Update Filesystem.java

--- Filesystem.java
+++ Filesystem.java
@@ -1,5 +1,6 @@
 package lab3;
 import java.io.*;
+import java.util.*;
 
 
 public class Filesystem 
@@ -11,8 +12,69 @@ public class Filesystem
 	}
 	
 	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-	public void runcmd()
+	public void runcmd() throws IOException
 	{
 		String line = in.readLine();
+		String cmd[] = line.split(" ");
+		
+		switch (cmd[0])
+		{
+		case "exit":
+			exit(cmd);
+			break;
+		
+		case "pwd":
+			pwd(cmd);
+			break;
+			
+		case "ls":
+			ls(cmd);
+			break;
+			
+		case "cd":
+			cd(cmd);
+			break;
+			
+		
+		}
+		
 	}
+	
+	private void exit(String[] cmd)
+	{
+		System.exit(0);
+	}
+	
+	private void pwd(String[] cmd)
+	{
+		try 
+		{
+			System.out.println(dir.getCanonicalPath()/* + " | " + dir.listFiles().length*/);
+			
+		} 
+		catch (IOException ex) 
+		{
+			// TODO Auto-generated catch block
+			ex.printStackTrace();
+		}
+	}
+	
+	private void ls(String[] cmd)
+	{
+		boolean l = false;
+		if(cmd.length > 1 && cmd[1].equals("-l"))
+			l = true;
+		
+		for(File file: dir.listFiles())
+			System.out.println(file.getName());
+	}
+	
+	private void cd(String[] cmd)
+	{
+		if(cmd[1].equals(".."))
+			dir = dir.getParentFile();
+		else
+			dir = new File(dir, cmd[1]);
+	}
+	
 }
